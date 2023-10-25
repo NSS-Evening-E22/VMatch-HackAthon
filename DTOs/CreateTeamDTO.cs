d0adb279fbf71a6c6f0370593dc4c5596d17930d
Thomas Pencarinha <tjpencarinha@gmail.com>
2023-10-24 20:49:57 -0400

    Created CRUD endpoints for teams, updated database schema

---
+++ DTOs/CreateTeamDTO.cs
@@ -0,0 +1,12 @@
+﻿using Volunteer_Match_Backend.Models;
+
+namespace Volunteer_Match_BE.DTOs
+{
+    public class CreateTeamDTO
+    {
+        public string VolunteerId { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Sponsor { get; set; }
+    }
+}
